6a558f3ede99bda18b15da8e4c2eb4af89390e63
lawrencemathenge <lawrencemathenge6@gmail.com>
2024-07-14 09:54:29 +0300

    my commit to code challlenge

---
+++ index.js
@@ -0,0 +1,23 @@
+const studentGrade = { 
+    studentFirstName: 'Alex',
+    studentSecndName:'Njoroge',
+    marks:70 ,
+    studentFirstName:'Ann',
+    studentSecondName:'Mboche',
+    marks:59 ,
+    studentFirstName:'Peter',
+    studentSecondName:'Mbali',
+    marks:49,
+}
+
+if(studentGrade.marks<=39) {
+    console.log('Grade:E'); }
+    else if (marks<=50) {
+        console.log('Grade:D'); }
+else if(studentGrade.marks<=60) {
+    console.log('Grade:c'); }
+    else if(marks<=70) {
+        console.log('Grade:B'); }
+        else if(studentGrade.marks<=80|| marks<=100) {
+        console.log('Grade:A'); }
+    
